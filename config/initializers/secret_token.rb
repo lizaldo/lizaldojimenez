9943328461f1dbd667743b5382fe726aa0731c71
root <root@ubuntu.(none)>
2013-03-17 11:50:52 -0400

    ¨baseline¨

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lizaldojimenez::Application.config.secret_token = '4eb57581e9957a4bfb448c5491bbd6971887ab7411399c96e3bda136162123576d7d0b3c360b756addef9ef3a574a17055d06d8281b32923f18ebcf685d85033'
